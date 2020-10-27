1c17164de197189012ab10e913a278533d345246
andres2811 <36390097+andres2811@users.noreply.github.com>
2020-10-27 11:29:54 -0500

    cambios
    capas

---
+++ hc_cauchos2/hccauchosAPI/Controllers/EmpleadoController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Utilitarios;
+using LogicaNegocio;
+using System;
+
+namespace hccauchosAPI.Controllers
+{
+    [Authorize(Roles = "2")]
+    [RoutePrefix("api/empleado")]
+    public class EmpleadoController : ApiController
+    {
+    }
+}
