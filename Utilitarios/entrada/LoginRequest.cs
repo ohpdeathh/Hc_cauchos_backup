3dc124fb1dcccb082eac6757ffe5ce604ee6e3ed
j2779 <70145306+j2779@users.noreply.github.com>
2020-11-03 10:33:24 -0500

    login

--- Utilitarios/entrada/LoginRequest.cs
+++ Utilitarios/entrada/LoginRequest.cs
@@ -14,4 +14,50 @@ namespace Utilitarios
         [Required]
         public int AplicacionId { get; set; }
     }
+    public class catalogocat
+    {
+        [Required(ErrorMessage = "necesita ingresar una categoria.")]
+        public int categoria { get; set; }
+    }
+    public class catalogomarca
+    {
+        [Required(ErrorMessage = "necesita ingresar una marca.")]
+        public int marca { get; set; }
+    }
+    public class catalogocombinado
+    {
+        [Required(ErrorMessage = "necesita ingresar una categoria.")]
+        public int categoria { get; set; }
+        [Required(ErrorMessage = "necesita ingresar una marca.")]
+        public int marca { get; set; }
+    }
+    public class catalogoprecio
+    {
+        [Required(ErrorMessage = "necesita ingresar un precio.")]
+        public string precio { get; set; }
+    }
+    public class catalogopreciocategoria
+    {
+            [Required(ErrorMessage = "necesita ingresar una categoria.")]
+            public int categoria { get; set; }
+            [Required(ErrorMessage = "necesita ingresar un precio.")]
+            public string precio { get; set; }
+    }
+    public class catalogopreciomarca
+    {
+        [Required(ErrorMessage = "necesita ingresar una marca.")]
+        public int marca { get; set; }
+        [Required(ErrorMessage = "necesita ingresar un precio.")]
+        public string precio { get; set; }
+    }
+    public class combinadorequest
+    {
+        [Required(ErrorMessage = "necesita ingresar una marca.")]
+        public int marca { get; set; }
+        [Required(ErrorMessage = "necesita ingresar una categoria.")]
+        public int categoria { get; set; }
+        [Required(ErrorMessage = "necesita ingresar un precio.")]
+        public string precio { get; set; }
+    }
+
 }
